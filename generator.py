b6792a7c6cb923308cdd907b660621558d7eaf46
Marco Pezzutti <marco.pezzutti@hotmail.it>
2015-02-03 01:10:34 +0100

    istanza circolare realizzata

--- generator.py
+++ generator.py
@@ -1,7 +1,7 @@
 # modulo generazione istanze
 import sys
 from random import randrange
-from math import fabs
+from math import fabs, ceil
 
 
 class Grid():
@@ -42,8 +42,9 @@ class Grid():
                 if col is not None:
                     print(col.num, end='\t')
                 else:
-                    print(col, end='\t')
+                    print("-", end='\t')
             print()
+        print()
 
     def insert_node(self, nodo):
         self.grid[nodo.x_pos][nodo.y_pos] = nodo
@@ -65,6 +66,7 @@ class Grid():
             for j in i:
                 print(j, end='\t')
             print()
+        print()
 
 
 class Node():
@@ -81,14 +83,65 @@ class Node():
         self.num = num
 
 
+class Generator():
+
+    """Generatore casuale di nodi.
+
+    Possiede gli attributi che rappresentano la griglia,
+    la quantità di nodida creare,
+    la distribuzione sulla griglia: casuale, circolare, cluster
+    """
+
+    def __init__(self, griglia, distr, num_nodi=0):
+        self.griglia = griglia
+        self.num_nodi = num_nodi
+        self.distr = distr
+
+    def crea_nodi(self):
+        if self.distr == "casuale":
+            pass
+        elif self.distr == "circolare":
+            cx = ceil(self.griglia.x_dim / 2) - 1
+            cy = ceil(self.griglia.y_dim / 2) - 1
+            r = min(cx + 1, cy + 1) - 1
+            num_nodi = r * 4
+            print("cx = %d, cy = %d, r = %d, numero nodi = %d" %
+                  (cx, cy, r, num_nodi))
+            cx -= r
+            nodo = Node(cx, cy, 0)
+            self.griglia.insert_node(nodo)
+            for i in range(1, num_nodi):
+                if i / r <= 1:
+                    cx += 1
+                    cy += 1
+                    nodo = Node(cx, cy, i)
+                    print("i = %d, cx = %d, cy = %d" % (i, cx, cy))
+                elif 1 < i / r <= 2:
+                    cx += 1
+                    cy -= 1
+                    nodo = Node(cx, cy, i)
+                    print("i = %d, cx = %d, cy = %d" % (i, cx, cy))
+                elif 2 < i / r <= 3:
+                    cx -= 1
+                    cy -= 1
+                    nodo = Node(cx, cy, i)
+                    print("i = %d, cx = %d, cy = %d" % (i, cx, cy))
+                elif 3 < i / r <= 4:
+                    cx -= 1
+                    cy += 1
+                    nodo = Node(cx, cy, i)
+                    print("i = %d, cx = %d, cy = %d" % (i, cx, cy))
+                self.griglia.insert_node(nodo)
+        elif self.distr == "cluster":
+            pass
+
+
 if __name__ == '__main__':
     x_dim = int(sys.argv[1])
     y_dim = int(sys.argv[2])
     g = Grid(x_dim, y_dim)
-    n = Node(2, 4, 0)
-    n1 = Node(0, 3, 1)
-    g.insert_node(n)
-    g.insert_node(n1)
+    gen = Generator(g, "circolare")
+    gen.crea_nodi()
     g.print_grid()
     g.manhattan_distance()
     g.print_weigths()
